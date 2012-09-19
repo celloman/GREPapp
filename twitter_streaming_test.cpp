412cc550b99c1c2c8070b679e1021c0d338f0eed
Gresham Schlect <gresham.schlect@gmail.com>
2012-09-18 17:18:09 -0700

    alchemy api added

--- twitter_streaming_test.cpp
+++ twitter_streaming_test.cpp
@@ -1,9 +1,12 @@
+#include <cstdlib>
 #include <iostream>
 #include <cstring>
 #include <curl/curl.h>
 #include <json/json.h>
 
 using namespace std;
+
+string buffer;
 
 string to_lowercase(string text) //convert a string to lowercase
 {
@@ -13,31 +16,85 @@ string to_lowercase(string text) //convert a string to lowercase
   return text;
 }
 
+size_t write_data(char *data, size_t size, size_t nmemb, void *usrdata)
+{
+  buffer = "";
+  buffer.append((char*) data, size * nmemb);
+  return size * nmemb;
+}
+
+double get_sentiment(string text, string subject)
+{
+  CURL *curl;
+  CURLcode res;
+
+  curl = curl_easy_init();
+
+  if(curl)
+  {
+    string url = "http://access.alchemyapi.com/calls/text/TextGetTargetedSentiment?apikey=3ab48469cc894436ef2ea8bf8f7bf76d8abb7cab&outputMode=json&text=";
+    url += curl_easy_escape(curl, text.c_str(), text.length());
+    url += "&target=";
+    url += subject;
+
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+    // Perform the request, res will get the return code
+    res = curl_easy_perform(curl);
+    // Check for errors
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    else
+    {
+      Json::Value root;
+      Json::Reader reader;
+
+      if(!reader.parse(buffer.c_str(), root) && root["status"].asString() == "ERROR")
+        cout << "Sentiment JSON fail!\n";
+      else
+      {
+        curl_easy_cleanup(curl);
+        return root["docSentiment"]["score"].asDouble();
+      }
+    }
+
+    curl_easy_cleanup(curl);
+  }
+  return 0;
+}
+
 size_t callback(char *data, size_t size, size_t nmemb, void *usrdata)
 {
-  static int republican = 0, democrat = 0;
-
+  static int republican = 0, democrat = 0, useful_tweets = 0, tweets = 0;
+  tweets++;
+  double sentiment;
   Json::Value root;
   Json::Reader reader;
 
   if(!reader.parse(data, root))
     cout << "JSON fail!\n";
 
-  cout << root["text"];
-  
   //convert tweet to lowercase so lowercase keywords can be searched for
   string tweet_in_lowercase = to_lowercase(root["text"].asString());
-  
-  cout << tweet_in_lowercase << endl; 
 
   //TODO: once we add more keywords we may want to move this functionality to a separate function
   //search the lowercase tweet for the following strings
   if (string::npos != tweet_in_lowercase.find("obama"))
-    democrat++;
+  {
+    sentiment = get_sentiment(root["text"].asString(), "obama");
+    democrat += sentiment;
+    if(sentiment != 2)
+      useful_tweets++;
+  }
   if (string::npos != tweet_in_lowercase.find("romney"))
-    republican++;
+  {
+    sentiment = get_sentiment(root["text"].asString(), "romney");
+    republican += sentiment;
+    if(sentiment != 2)
+      useful_tweets++;
+  }
 
-  cout << "Romney: " << republican << ", Obama: " << democrat << "\n\n";
+  cout << "Romney: " << republican << ", Obama: " << democrat << " (" << useful_tweets << "/" << tweets << ")\n";
 
   return size*nmemb;
 }
