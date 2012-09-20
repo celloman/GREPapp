d7de3c40f8450885a17c72731623a73b480a0e48
Gresham Schlect <gresham.schlect@gmail.com>
2012-09-19 21:35:33 -0700

    split .h and .cpp files

---
+++ twitter_stream.cpp
@@ -0,0 +1,81 @@
+#include "twitter_stream.h"
+
+size_t write_function(char *data, size_t size, size_t nmemb, void *usrdata)
+{
+	if(data != NULL)
+	{
+		twitter_stream *ts = (twitter_stream*)(usrdata);
+		tweet t = tweet(data);
+		ts->m_callback(t);
+	}
+	else
+	{
+		cerr << "null json string returned from twitter...\n";
+	}
+	
+	return size * nmemb;
+}
+
+// convert a string to lowercase
+string tweet::to_lowercase(string text)
+{
+	for(int i = 0; i < text.length(); ++i)
+		text[i] = tolower(text[i]);
+
+	return text;
+}
+
+// constructor (takes json data)
+tweet::tweet(char *json_data)
+{
+	Json::Value root;
+	Json::Reader reader;
+	if(json_data == NULL || !reader.parse(json_data, root))
+	{
+		cerr << "failed to decode tweet JSON!\n";
+		exit(-1);
+	}
+	else
+	{
+		m_text = to_lowercase(root["text"].asString());
+	}
+}
+
+// constructor (takes callback function and keywords)
+twitter_stream::twitter_stream(void (*callback)(tweet))
+{
+	m_callback = callback;
+	//m_keywords = keywords;
+}
+
+// start the curl request
+bool twitter_stream::start()
+{
+	CURLcode res;
+
+	m_curl = curl_easy_init();
+
+	if(m_curl)
+	{
+		curl_easy_setopt(m_curl, CURLOPT_URL, "https://stream.twitter.com/1/statuses/filter.json");
+		curl_easy_setopt(m_curl, CURLOPT_POST, 1);
+		curl_easy_setopt(m_curl, CURLOPT_POSTFIELDS, "track=obama,romney");
+		curl_easy_setopt(m_curl, CURLOPT_USERPWD, "vikings383:383vikings");
+		curl_easy_setopt(m_curl, CURLOPT_WRITEDATA, this);
+		curl_easy_setopt(m_curl, CURLOPT_WRITEFUNCTION, write_function);
+		// Perform the request, res will get the return code
+		res = curl_easy_perform(m_curl);
+		// Check for errors
+		if(res != CURLE_OK)
+		{
+			cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res);
+			return false;
+		}
+		
+		curl_easy_cleanup(m_curl);
+	}
+	else
+	{
+		return false;
+	}
+}
