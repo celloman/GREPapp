6090bd75993e7d3ea227dc569ef9409d7eb2478f
Gresham Schlect <gresham.schlect@gmail.com>
2013-01-26 23:04:53 -0800

    stubbed out project
    includes the activities that we decided on (1/26 meeting) and the
    twitter4j library

---
+++ TweetSpeak/src/grep/app/TopicActivity.java
@@ -0,0 +1,22 @@
+package grep.app;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TopicActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_topic);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_topic, menu);
+		return true;
+	}
+
+}
