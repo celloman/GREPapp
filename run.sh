4a1cd986fc0abd72a3fafb9e9fbec4649b04f8a0
Everett <el3loch@gmail.com>
2013-01-26 17:05:22 -0800

    Clean repo

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./get_tweets | ./get_weight | ./get_sentiment | ./aggregate | ./update.sh
--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "{\"gauge\":50, \"tweets\":0, \"liberal\":0, \"conservative\":0, \"time\":0}" > "html/out.txt"
-echo "{\"id\":0, \"text\":\"\", \"title\":\"\", \"type\":\"\"}" > "html/out.txt"
-
-while read json; do
-	echo $json > "html/out.txt"
-done
