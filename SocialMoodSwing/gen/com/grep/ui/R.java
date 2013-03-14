5d1ddcb80c10fc567909bfa3d4df13c242b17e3f
Everett <el3loch@gmail.com>
2013-03-13 22:40:06 -0700

    Database is implemented and functioning adequately
    Database has been created and has pased basic unit tests done on each method.
    Some things that still need to be implemented are:
    	- Improve read/write efficiency (currently databse is opened
    		and closed on every call)
    	- Additional test need to be done to ensure any potential out of bounds
    		errors are taken care of

--- SocialMoodSwing/gen/com/grep/ui/R.java
+++ SocialMoodSwing/gen/com/grep/ui/R.java
@@ -24,17 +24,19 @@ public final class R {
         public static final int username=0x7f070003;
     }
     public static final class layout {
-        public static final int activity_gauge=0x7f030000;
-        public static final int activity_topic=0x7f030001;
-        public static final int activity_topic_list=0x7f030002;
-        public static final int keyword_dialog=0x7f030003;
-        public static final int login_dialog=0x7f030004;
-        public static final int warning_dialog=0x7f030005;
+        public static final int activity_dbunit_test=0x7f030000;
+        public static final int activity_gauge=0x7f030001;
+        public static final int activity_topic=0x7f030002;
+        public static final int activity_topic_list=0x7f030003;
+        public static final int keyword_dialog=0x7f030004;
+        public static final int login_dialog=0x7f030005;
+        public static final int warning_dialog=0x7f030006;
     }
     public static final class menu {
-        public static final int activity_gauge=0x7f060000;
-        public static final int activity_topic=0x7f060001;
-        public static final int activity_topic_list=0x7f060002;
+        public static final int activity_dbunit_test=0x7f060000;
+        public static final int activity_gauge=0x7f060001;
+        public static final int activity_topic=0x7f060002;
+        public static final int activity_topic_list=0x7f060003;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
