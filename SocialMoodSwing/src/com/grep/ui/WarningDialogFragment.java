a7fa64b2207e755fb97dcc6778f89aa86a7b1184
Gresham Schlect <gresham.schlect@gmail.com>
2013-03-17 20:31:12 -0700

    minor change in warning dialog

--- SocialMoodSwing/src/com/grep/ui/WarningDialogFragment.java
+++ SocialMoodSwing/src/com/grep/ui/WarningDialogFragment.java
@@ -8,8 +8,6 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * WarningDialogFragment displays a warning to the user.
