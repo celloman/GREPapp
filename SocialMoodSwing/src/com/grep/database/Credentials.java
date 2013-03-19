53c636c92eeeb0fa1a86fd5a6b325ca84d456340
Everett <el3loch@gmail.com>
2013-03-19 14:53:57 -0700

    commented Credentials and Keyword classes

--- SocialMoodSwing/src/com/grep/database/Credentials.java
+++ SocialMoodSwing/src/com/grep/database/Credentials.java
@@ -1,27 +1,60 @@
+/**
+ * Credentials.Java
+ * 
+ * @author Gresham, Ryan, Everett, Pierce
+ *
+ */
+
 package com.grep.database;
 
+/**
+ *  public class Credentials 
+ *  <br><br>
+ *  Utility class containing OAuth credentials used in SQLite database
+ */
 public class Credentials {
 	
 	// Local private variables
 	// Table key id
+	/**
+	 * private int id
+	 */
 	private int id;
 	
 	// OAuth consumer key and secret
+	/**
+	 * private String consumerKey
+	 */
 	private String consumerKey;
+	
+	/**
+	 * private String consumerSecret
+	 */
 	private String consumerSecret;
 	
-	// constructor
+	/**
+	 * Constructor
+	 */
 	public Credentials() {
 		//empty
 	}
 	
-	// constructor
+	/**
+	 * Constructor
+	 * @param key 	(String)
+	 * @param secret 	(String)
+	 */
 	public Credentials(String key, String secret) {
 		this.consumerKey = key;
 		this.consumerSecret = secret;
 	}
 	
-	// constructor
+	/**
+	 * Constructor
+	 * @param id	(int)
+	 * @param key	(String)
+	 * @param secret	(String)
+	 */
 	public Credentials(int id, String key, String secret) {
 		this.id = id;
 		this.consumerKey = key;
@@ -30,26 +63,50 @@ public class Credentials {
 	
 	// Get and set methods
 	
+	/**
+	 * public int getId
+	 * @return id
+	 */
 	public int getId() {
 		return this.id;
 	}
 	
+	/**
+	 * public void setId
+	 * @param id	(int)
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 	
+	/**
+	 * public String getConsumerKey
+	 * @return consumerKey
+	 */
 	public String getConsumerKey() {
 		return this.consumerKey;
 	}
 	
+	/**
+	 * public void setConsumerKey
+	 * @param key	(String)
+	 */
 	public void setConsumerKey(String key) {
 		this.consumerKey = key;
 	}
 	
+	/**
+	 * public String getConsumerSecret
+	 * @return consumerSecret
+	 */
 	public String getConsumerSecret() {
 		return this.consumerSecret;
 	}
 	
+	/**
+	 * setConsumerSecret
+	 * @param secret	(String)
+	 */
 	public void setConsumerSecret(String secret) {
 		this.consumerSecret	= secret;
 	}
--- SocialMoodSwing/src/com/grep/database/Keyword.java
+++ SocialMoodSwing/src/com/grep/database/Keyword.java
@@ -1,28 +1,61 @@
+/**
+ * Keyword.java
+ * 
+ * @author Gresham, Ryan, Everett, Pierce
+ */
+
 package com.grep.database;
 
+/**
+ * public class Keyword
+ * <br><br>
+ * Utility class containing keywords used in SQLite database
+ */
 public class Keyword {
 	
 	// Local private variables
 	// Table key id
+	/**
+	 * private int id
+	 */
 	private int id;
 	
+	/**
+	 * private String keyword
+	 */
 	private String keyword;
 	
 	// Foreign topic id for keyword
+	/**
+	 * private int t_id
+	 * <br><br>
+	 * Topic ID, foreign key reference
+	 */
 	private int t_id;
 	
-	// constructor
+	/**
+	 * Constructor
+	 */
 	public Keyword() {
 		//empty
 	}
 	
-	// constructor, sets keyword field
+	/**
+	 * Constructor
+	 * @param topic_id
+	 * @param keyword
+	 */
 	public Keyword(int topic_id, String keyword) {
 		this.t_id = topic_id;
 		this.keyword = keyword;
 	}
 	
-	// constructor, sets all fields
+	/**
+	 * Constructor
+	 * @param id
+	 * @param topic_id
+	 * @param keyword
+	 */
 	public Keyword(int id, int topic_id, String keyword) {
 		this.id = id;
 		this.t_id = topic_id;		
@@ -31,26 +64,50 @@ public class Keyword {
 	
 	// Get and set methods
 	
+	/**
+	 * public int getId
+	 * @return id
+	 */
 	public int getId() {
 		return this.id;
 	}
 	
+	/**
+	 * public void setId
+	 * @param id (int)
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 	
+	/**
+	 * public String getKeyword
+	 * @return keyword
+	 */
 	public String getKeyword() {
 		return this.keyword;
 	}
 	
+	/**
+	 * public void setKeyword
+	 * @param keyword (String)
+	 */
 	public void setKeyword(String keyword) {
 		this.keyword = keyword;
 	}
 	
+	/**
+	 * public int getKeywordTopicId
+	 * @return t_id
+	 */
 	public int getKeywordTopicId() {
 		return this.t_id;
 	}
 	
+	/**
+	 * public void setKeywordTopicId
+	 * @param id (int)
+	 */
 	public void setKeywordTopicId(int id) {
 		this.t_id = id;
 	}
