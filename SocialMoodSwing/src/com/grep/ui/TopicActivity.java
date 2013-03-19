5c4cb3f8897f13036d9b663904e3553bb3178877
Pierce Trey <cellomanpierce@gmail.com>
2013-03-19 16:45:45 -0700

    Basic Graph Implemented
    From androidplot site, adapted to project, placed in topicactivity

--- SocialMoodSwing/src/com/grep/ui/TopicActivity.java
+++ SocialMoodSwing/src/com/grep/ui/TopicActivity.java
@@ -1,6 +1,12 @@
 package com.grep.ui;
 
+import java.util.Arrays;
+
+import com.androidplot.series.XYSeries;
+import com.androidplot.xy.*;
+
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
@@ -23,6 +29,45 @@ public class TopicActivity extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_topic);
 		setTitle(R.string.title_activity_topic);
+		
+		final XYPlot mySimpleXYPlot;
+		
+		// initialize our XYPlot reference:
+        mySimpleXYPlot = (XYPlot) findViewById(R.id.mySimpleXYPlot);
+
+        // Create a couple arrays of y-values to plot:
+        Number[] series1Numbers = {1, 8, 5, 2, 7, 4};
+        Number[] series2Numbers = {4, 6, 3, 8, 2, 10};
+
+        // Turn the above arrays into XYSeries':
+        XYSeries series1 = new SimpleXYSeries(
+                Arrays.asList(series1Numbers),          // SimpleXYSeries takes a List so turn our array into a List
+                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Y_VALS_ONLY means use the element index as the x value
+                "Series1");                             // Set the display title of the series
+
+        // same as above
+        XYSeries series2 = new SimpleXYSeries(Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
+
+        // Create a formatter to use for drawing a series using LineAndPointRenderer:
+        LineAndPointFormatter series1Format = new LineAndPointFormatter(
+                Color.rgb(0, 200, 0),                   // line color
+                Color.rgb(0, 100, 0),                   // point color
+                null,                                   // fill color (none)
+                new PointLabelFormatter(Color.WHITE));                           // text color
+
+        // add a new series' to the xyplot:
+        mySimpleXYPlot.addSeries(series1, series1Format);
+
+        // same as above:
+        mySimpleXYPlot.addSeries(series2,
+                new LineAndPointFormatter(
+                        Color.rgb(0, 0, 200),
+                        Color.rgb(0, 0, 100),
+                        null,
+                        new PointLabelFormatter(Color.WHITE)));
+
+        // reduce the number of range labels
+        mySimpleXYPlot.setTicksPerRangeLabel(3);
 	}
 
 	@Override
